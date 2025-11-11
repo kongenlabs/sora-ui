fb160bde55f4b593477305d7637f1c212fea2ad0
bakansm <khuynhbao5@gmail.com>
2025-11-11 23:28:33 +0700

    chore(typography): add utilities layer for typography

--- src/components/ui/primitive/typography.tsx
+++ src/components/ui/primitive/typography.tsx
@@ -4,84 +4,27 @@ import type { ComponentProps } from 'react';
 import { cn } from '@/utils/tailwind';
 
 export function H1({ className, ...props }: ComponentProps<'h1'>) {
-  return (
-    <h1
-      className={cn(
-        'font-extrabold font-sans text-h1-sm leading-tight tracking-tighter',
-        'md:text-h1-md',
-        'xl:text-h1-lg',
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <h1 className={cn('font-extrabold font-sans text-h1 leading-tight tracking-tighter', className)} {...props} />;
 }
 
 export function H2({ className, ...props }: ComponentProps<'h2'>) {
-  return (
-    <h2
-      className={cn(
-        'font-bold font-sans text-h2-sm leading-tight tracking-tight',
-        'md:text-h2-md',
-        'xl:text-h2-lg',
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <h2 className={cn('font-bold font-sans text-h2 leading-tight tracking-tight', className)} {...props} />;
 }
+
 export function H3({ className, ...props }: ComponentProps<'h3'>) {
-  return (
-    <h3
-      className={cn(
-        'font-sans font-semibold text-h3-sm leading-snug tracking-tight',
-        'md:text-h3-md',
-        'xl:text-h3-lg',
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <h3 className={cn('font-sans font-semibold text-h3 leading-snug tracking-tight', className)} {...props} />;
 }
+
 export function H4({ className, ...props }: ComponentProps<'h4'>) {
-  return (
-    <h4
-      className={cn(
-        'font-sans font-semibold text-h4-sm leading-snug tracking-normal',
-        'md:text-h4-md',
-        'xl:text-h4-lg',
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <h4 className={cn('font-sans font-semibold text-h4 leading-snug tracking-normal', className)} {...props} />;
 }
+
 export function H5({ className, ...props }: ComponentProps<'h5'>) {
-  return (
-    <h5
-      className={cn(
-        'font-medium font-sans text-h5-sm leading-normal tracking-normal',
-        'md:text-h5-md',
-        'xl:text-h5-lg',
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <h5 className={cn('font-medium font-sans text-h5 leading-normal tracking-normal', className)} {...props} />;
 }
 
 export function H6({ className, ...props }: ComponentProps<'h6'>) {
-  return (
-    <h6
-      className={cn(
-        'font-medium font-sans text-h6-sm leading-normal tracking-wide',
-        'md:text-h6-md',
-        'xl:text-h6-lg',
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <h6 className={cn('font-medium font-sans text-h6 leading-normal tracking-wide', className)} {...props} />;
 }
 
 export function Paragraph({ className, ...props }: ComponentProps<'p'>) {
@@ -106,12 +49,7 @@ export function Emphasis({ className, ...props }: ComponentProps<'em'>) {
 }
 
 export function Small({ className, ...props }: ComponentProps<'small'>) {
-  return (
-    <small
-      className={cn('text-small-sm leading-relaxed tracking-wide', 'md:text-small-md', 'xl:text-small-lg', className)}
-      {...props}
-    />
-  );
+  return <small className={cn('text-small leading-relaxed tracking-wide', className)} {...props} />;
 }
 
 export function Blockquote({ className, ...props }: ComponentProps<'blockquote'>) {
