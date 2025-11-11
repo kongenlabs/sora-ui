123f33032afa8e4d58de24455ab4acc7bc929b3d
bakansm <khuynhbao5@gmail.com>
2025-11-11 22:17:32 +0700

    chore(button): add tooltip, size and icon variant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
+'use client';
+
+import { LoaderCircleIcon, PlusIcon, SendHorizonalIcon } from 'lucide-react';
+import { useState } from 'react';
+import { Button } from '@/components/ui/primitive/button';
+
 export default function Home() {
-  return null;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  return (
+    <div className='flex items-start gap-2 p-10'>
+      <Button size={'sm'}>
+        Click <SendHorizonalIcon />
+      </Button>
+      <Button>
+        Click <SendHorizonalIcon />
+      </Button>
+      <Button color={'primary-5'} size={'lg'}>
+        Click <SendHorizonalIcon />
+      </Button>
+      <Button
+        color={'primary-5'}
+        disabled={isLoading}
+        onClick={() => {
+          setIsLoading(true);
+        }}
+      >
+        Click {isLoading ? <LoaderCircleIcon className='animate-spin' /> : <SendHorizonalIcon />}
+      </Button>
+      <Button icon>
+        <PlusIcon />
+      </Button>
+      <Button color={'primary-1'} icon size={'sm'}>
+        <PlusIcon />
+      </Button>
+      <Button color={'primary-1'} icon variant={'plain'}>
+        <PlusIcon />
+      </Button>
+      <Button color={'primary-3'} icon size={'lg'}>
+        <PlusIcon />
+      </Button>
+      <Button color={'primary-4'} icon>
+        <PlusIcon />
+      </Button>
+      <Button color={'primary-5'} icon>
+        <PlusIcon />
+      </Button>
+    </div>
+  );
 }
--- src/components/ui/primitive/button.tsx
+++ src/components/ui/primitive/button.tsx
@@ -1,101 +1,170 @@
-import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { cva, cx, type VariantProps } from 'class-variance-authority';
 import type { ComponentProps } from 'react';
-import { cn } from '@/utils/tailwind';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip';
 
 const buttonVariants = cva(
-  cn(
-    'flex cursor-pointer items-center justify-center whitespace-nowrap font-medium leading-none transition-all duration-150',
-    'focus-visible:outline-2 focus-visible:outline-offset-2',
-    "[&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+  cx(
+    'relative inline-flex shrink-0 cursor-pointer items-center justify-center whitespace-nowrap font-medium leading-none transition-all duration-150',
+    '[&_svg]:pointer-events-none [&_svg]:shrink-0',
+    'focus-visible:outline-3 focus-visible:outline-offset-1',
+    'disabled:translate-none disabled:cursor-not-allowed disabled:opacity-70 disabled:shadow-none',
+    'hover:brightness-95',
+    'active:brightness-90',
   ),
   {
+    compoundVariants: [
+      {
+        className: 'px-2.5 py-1.5',
+        icon: false,
+        size: 'sm',
+      },
+      {
+        className: 'px-3 py-2',
+        icon: false,
+        size: 'md',
+      },
+      {
+        className: 'px-3.5 py-2.5',
+        icon: false,
+        size: 'lg',
+      },
+      {
+        className: 'p-1.5',
+        icon: true,
+        size: 'sm',
+      },
+      {
+        className: 'p-2',
+        icon: true,
+        size: 'md',
+      },
+      {
+        className: 'p-2.5',
+        icon: true,
+        size: 'lg',
+      },
+    ],
     defaultVariants: {
-      color: 'primary-3',
+      color: 'primary-4',
+      icon: false,
       size: 'md',
       variant: 'raised',
     },
     variants: {
       color: {
-        'primary-1': cn(
+        'primary-1': cx(
           'border-primary-800 bg-primary-300 text-primary-950 shadow-primary-900',
-          'focus:outline-primary-950',
+          'focus:outline-primary-600/80',
         ),
-        'primary-2': cn(
+        'primary-2': cx(
           'border-primary-800 bg-primary-400 text-primary-950 shadow-primary-900',
-          'focus-visible:outline-primary-950',
+          'focus:outline-primary-600/80',
         ),
-        'primary-3': cn(
+        'primary-3': cx(
           'border-primary-800 bg-primary-500 text-primary-950 shadow-primary-900',
-          'focus-visible:outline-primary-950',
+          'focus:outline-primary-600/80',
         ),
-        'primary-4': cn(
-          'border-primary-800 bg-primary-600 text-primary-50 shadow-primary-900',
-          'focus-visible:outline-primary-950',
+        'primary-4': cx(
+          'border-primary-800 bg-primary-600 text-primary-950 shadow-primary-900',
+          'focus:outline-primary-600/80',
         ),
-        'primary-5': cn(
+        'primary-5': cx(
           'border-primary-800 bg-primary-700 text-primary-50 shadow-primary-900',
-          'focus-visible:outline-primary-950',
+          'focus:outline-primary-600/80',
         ),
-        'secondary-1': cn(
+        'secondary-1': cx(
           'border-secondary-800 bg-secondary-300 text-secondary-950 shadow-secondary-900',
-          'focus-visible:outline-secondary-950',
+          'focus:outline-secondary-600/80',
         ),
-        'secondary-2': cn(
+        'secondary-2': cx(
           'border-secondary-800 bg-secondary-400 text-secondary-950 shadow-secondary-900',
-          'focus-visible:outline-secondary-950',
+          'focus:outline-secondary-600/80',
         ),
-        'secondary-3': cn(
+        'secondary-3': cx(
           'border-secondary-800 bg-secondary-500 text-secondary-950 shadow-secondary-900',
-          'focus-visible:outline-secondary-950',
+          'focus:outline-secondary-600/80',
         ),
-        'secondary-4': cn(
+        'secondary-4': cx(
           'border-secondary-800 bg-secondary-600 text-secondary-50 shadow-secondary-900',
-          'focus-visible:outline-secondary-950',
+          'focus:outline-secondary-600/80',
         ),
-        'secondary-5': cn(
+        'secondary-5': cx(
           'border-secondary-800 bg-secondary-700 text-secondary-50 shadow-secondary-900',
-          'focus-visible:outline-secondary-950',
+          'focus:outline-secondary-600/80',
         ),
       },
+      icon: {
+        false: '',
+        true: '',
+      },
+
       size: {
-        lg: 'h-11 rounded-lg px-5.5 text-button-lg',
-        md: 'h-10 rounded px-5 text-button-md',
-        sm: 'h-9 rounded-sm px-4.5 text-button-sm',
+        lg: cx('gap-2 rounded-lg text-button-lg', "[&_svg:not([class*='size-'])]:size-4.5"),
+        md: cx('gap-1.5 rounded text-button-md', "[&_svg:not([class*='size-'])]:size-4"),
+        sm: cx('gap-1 rounded-sm text-button-sm', "[&_svg:not([class*='size-'])]:size-3.5"),
       },
       variant: {
-        elevated: cn(
-          '-translate-y-1 border-2 shadow-sm',
-          'hover:brightness-95',
-          'active:translate-none active:shadow-none active:brightness-90',
-        ),
-        flat: cn('border-2', 'hover:brightness-95', 'active:brightness-90'),
+        elevated: cx('-translate-y-1 border-2 shadow-sm', 'active:translate-none active:shadow-none'),
+        flat: 'border-2',
         plain: '',
-        raised: cn(
+        raised: cx(
           'border-2',
           'focus-visible:-translate-y-1 focus-visible:shadow-sm',
-          'hover:-translate-y-1 hover:shadow-sm hover:brightness-95',
-          'active:translate-none active:shadow-none active:brightness-90',
+          'hover:-translate-y-1 hover:shadow-sm',
+          'active:translate-none active:shadow-none',
         ),
       },
     },
   },
 );
 
-function Button({
+type ButtonVariantProps = VariantProps<typeof buttonVariants>;
+
+type ButtonProps = ComponentProps<'button'> &
+  ButtonVariantProps & {
+    tooltip?: string;
+    tooltipOptions?: ComponentProps<typeof Tooltip>;
+    tooltipContentOptions?: ComponentProps<typeof TooltipContent>;
+    tooltipProviderOptions?: ComponentProps<typeof TooltipProvider>;
+  };
+
+export function Button({
   className,
+  icon,
   variant,
   color,
   size,
-  asChild = false,
+  disabled,
+  tooltip,
+  tooltipOptions,
+  tooltipContentOptions,
+  tooltipProviderOptions,
   ...props
-}: ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
-  const Comp = asChild ? Slot : 'button';
+}: ButtonProps) {
+  const button = (
+    <button
+      className={cx(
+        buttonVariants({
+          color,
+          icon,
+          size,
+          variant,
+        }),
+        className,
+      )}
+      disabled={disabled}
+      {...props}
+    />
+  );
 
-  return <Comp className={cn(buttonVariants({ className, color, size, variant }))} data-slot='button' {...props} />;
+  if (!tooltip) return button;
+
+  return (
+    <TooltipProvider delayDuration={100} {...tooltipProviderOptions}>
+      <Tooltip {...tooltipOptions}>
+        <TooltipTrigger asChild>{button}</TooltipTrigger>
+        <TooltipContent {...tooltipContentOptions}>{tooltip}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 }
-
-export { Button, buttonVariants };
