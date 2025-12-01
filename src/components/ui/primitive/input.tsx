3bc84c6a0e31f1514878e7ebf7685d8e3a0a1b46
bakansm <khuynhbao5@gmail.com>
2025-12-01 18:05:05 +0700

    feat: Add `focus-within` placeholder opacity and apply background colors to `data-color` inputs by default.

--- src/components/ui/primitive/input.tsx
+++ src/components/ui/primitive/input.tsx
@@ -12,22 +12,20 @@ function Input({ className, type, color = 'primary', ...props }: InputProps) {
       className={cx(
         'h-10 w-full min-w-0 rounded border-2 bg-background px-3 py-1 text-p outline-none transition-colors',
         'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+        'focus-within:placeholder:opacity-0',
         'file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm',
 
         // Primary color
-        'data-[color=primary]:border-primary-border data-[color=primary]:text-on-primary-container data-[color=primary]:placeholder:text-on-primary-container/30',
+        'data-[color=primary]:border-primary-border data-[color=primary]:bg-primary-container data-[color=primary]:text-on-primary-container data-[color=primary]:placeholder:text-on-primary-container/30',
         'data-[color=primary]:selection:bg-primary data-[color=primary]:selection:text-on-primary',
-        'data-[color=primary]:focus-visible:border-primary-border data-[color=primary]:focus-visible:bg-primary-container',
 
         // Secondary color
-        'data-[color=secondary]:border-secondary-border data-[color=secondary]:text-on-secondary-container data-[color=secondary]:placeholder:text-on-secondary-container/30',
+        'data-[color=secondary]:border-secondary-border data-[color=secondary]:bg-secondary-container data-[color=secondary]:text-on-secondary-container data-[color=secondary]:placeholder:text-on-secondary-container/30',
         'data-[color=secondary]:selection:bg-secondary data-[color=secondary]:selection:text-on-secondary',
-        'data-[color=secondary]:focus-visible:border-secondary-border data-[color=secondary]:focus-visible:bg-secondary-container',
 
         // Tertiary color
-        'data-[color=tertiary]:border-tertiary-border data-[color=tertiary]:text-on-tertiary-container data-[color=tertiary]:placeholder:text-on-tertiary-container/30',
+        'data-[color=tertiary]:border-tertiary-border data-[color=tertiary]:bg-tertiary-container data-[color=tertiary]:text-on-tertiary-container data-[color=tertiary]:placeholder:text-on-tertiary-container/30',
         'data-[color=tertiary]:selection:bg-tertiary data-[color=tertiary]:selection:text-on-tertiary',
-        'data-[color=tertiary]:focus-visible:border-tertiary-border data-[color=tertiary]:focus-visible:bg-tertiary-container',
         className,
       )}
       data-color={color}
